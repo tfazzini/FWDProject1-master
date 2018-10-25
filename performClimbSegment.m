77e6fae678253d4fa280877e298a21e75aebdbf5
tfazzini <44451626+tfazzini@users.noreply.github.com>
2018-10-24 22:19:18 -0400

    THIS IS TRASH

--- performClimbSegment.m
+++ performClimbSegment.m
@@ -24,12 +24,12 @@ K2 = aero.K2;
 [CD0, K1] = adjustAero(M0, CD0, K1);
 
 q = (1/2)*rho*(avgV^2);
-if BC == true
-    [~,~,CL,CD] = calcBCAandBCM(S, WTO, Wi);
-else
-    LD = aero.LDmax;
-    CD = Wi/(q*S);
-    CL = LD*CD;
+% if BC == true
+%     [~,~,CL,CD] = calcBCAandBCM(S, WTO, Wi);
+% else
+%     LD = aero.LDmax;
+%     CD = Wi/(q*S);
+%     CL = LD*CD;
 %     delta = calcDeltaFromAlt(avgh);
 %     CL = (2*beta*(WTO/S))/(gamma*Pstd*delta*(M^2));
 %     CD = ((K1*CL^2) + (K2*CL) + CD0);
@@ -37,9 +37,9 @@ end
 
 % Calculating weight fraction
 u = (CD/CL)*(beta/alpha)*(WTO/T_SL);
-if u > 1
-    x = 5
-end
+% if u > 1
+%     x = 5
+% end
 
 Wf_Wi = exp(-(TSFC/avgV)*((deltah + dV2g)/(1 - u)));
 
