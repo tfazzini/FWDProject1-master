6d1e80f650bd710312dd6fe9a89ee80c86b893de
tfazzini <44451626+tfazzini@users.noreply.github.com>
2018-10-24 20:44:31 -0400

    Update calcThrustLapse.m

--- calcThrustLapse.m
+++ calcThrustLapse.m
@@ -24,7 +24,7 @@ if strcmp(engineType, 'turbojet') == 1
         alpha1 = 0.8*d_0*(1-0.16*sqrt(Mach));
 %         alpha2 = 0.76*(0.907 + 0.262*(abs(Mach-0.5)^1.5))*sigma^0.7;
 %         alpha = max(alpha1,alpha2);
-        alpha = alpha1;
+        alpha = alpha1; 
     end   
 else 
 if strcmp(engineType, 'turbofan_low') == 1
