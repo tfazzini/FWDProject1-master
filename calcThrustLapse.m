b4ddee5b9e646684709fd6a911e0b36b8a203bc3
tfazzini <44451626+tfazzini@users.noreply.github.com>
2018-10-24 20:38:42 -0400

    i fix it

--- calcThrustLapse.m
+++ calcThrustLapse.m
@@ -16,13 +16,15 @@ end
 if strcmp(engineType, 'turbojet') == 1
     % Checking whether afterburners used
     if burner == true
-        alpha1 = 0.8*d_0*(1-0.3*(theta_0-1)-0.1*sqrt(Mach));
-        alpha2 = (0.952 + (0.3*(Mach - 0.4)^2)) * sigma^0.7;
-        alpha = max(alpha1,alpha2);
+        alpha1 = d_0*(1-0.3*(theta_0-1)-0.1*sqrt(Mach));
+%         alpha2 = (0.952 + (0.3*(Mach - 0.4)^2)) * sigma^0.7;
+%         alpha = max(alpha1,alpha2);
+        alpha = alpha1;
     else
         alpha1 = 0.8*d_0*(1-0.16*sqrt(Mach));
-        alpha2 = 0.76*(0.907 + 0.262*(abs(Mach-0.5)^1.5))*sigma^0.7;
-        alpha = max(alpha1,alpha2);
+%         alpha2 = 0.76*(0.907 + 0.262*(abs(Mach-0.5)^1.5))*sigma^0.7;
+%         alpha = max(alpha1,alpha2);
+        alpha = alpha1;
     end   
 else 
 if strcmp(engineType, 'turbofan_low') == 1
